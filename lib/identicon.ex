35ae3ef5fcf0d0656d8625ee42ed6e98d5d95b81
Maria Alonso <maria.alonso@cabify.com>
2018-12-07 12:17:05 +0100

    separeted the grid in evens to color

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,23 +5,35 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
   end
 
-  def build_grid(image) do
-    %Identicon.Image{hex: hex} = image
-    hex
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    Enum.filter grid, fn({code, _index}) -> 
+      rem(code, 2) == 0  
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do  
+    grid = 
+      hex
+        |> Enum.chunk(3)
+        |> Enum.map(&mirror_row/1)
+        |> List.flatten
+        |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
     [first, second | _tail] = row 
+
     row ++ [second, first]
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
   
